575c0fcbd8ac39fe33fbf4937d74e93d3cfc9c52
harshi1122 <harshitha1155@gmail.com>
2022-05-26 18:28:46 +0530

    'deleting-a-model'

--- info/admin.py
+++ info/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Contact, Student, Take_attendance
+from .models import Student, Take_attendance
 # Register your models here.
 
-admin.site.register(Contact)
+
 admin.site.register(Student)
 admin.site.register(Take_attendance)
